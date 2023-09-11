15172a411f4dbfcf4eee6f1fe46bfa79cc3c7547
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2023-09-12 00:14:05 +0300

    setting animation on text when trigger is active

--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -1,115 +1,121 @@
-"use client"
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 import { gsap } from "gsap";
 import { Tween } from "react-gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import Link from "next/link";
 
 const Projects = () => {
+  const [isTriggerActive, setIsTriggerActive] = useState(false);
+  console.log(isTriggerActive)
   return (
-    <div className=' w-full bg-red-500 py-16 px-5'>
+    <div className=" w-full  py-16 px-5">
       <div className="mx-auto w-[95%]">
-        <div className="flex justify-between gallery">
-       
-          <div className="detailsWrapper left">
-            <div className="details h-[100vh] flex items-center">
-              <div className="">
-                <div className="heading bg-green-500 h-6 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-              </div>
-            </div>
-            <div className="details h-[100vh] flex items-center">
-              <div className="">
-                <div className="heading bg-green-500 h-6 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-              </div>
-            </div>
-            <div className="details h-[100vh] flex items-center">
-              <div className="">
-                <div className="heading bg-green-500 h-6 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-              </div>
-            </div>
-            <div className="details h-[100vh] flex items-center">
-              <div className="">
-                <div className="heading bg-green-500 h-6 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-              </div>
-            </div>
-            <div className="details h-[100vh] flex items-center">
-              <div className="">
-                <div className="heading bg-green-500 h-6 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-              </div>
-            </div>
-            <div className="details h-[100vh] flex items-center">
-              <div className="">
-                <div className="heading bg-green-500 h-6 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-              </div>
-            </div>
-            <div className="details h-[100vh] flex items-center">
-              <div className="">
-                <div className="heading bg-green-500 h-6 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-              </div>
-            </div>
-            <div className="details h-[100vh] flex items-center">
-              <div className="">
-                <div className="heading bg-green-500 h-6 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-                <div className="text bg-green-100 h-3 w-80 my-2"></div>
-              </div>
-            </div>
-           
-          </div>
-       
-          <div className="right flex items-center h-[100vh]">
-            <Tween
-           
-            to={{
-             
-              scrollTrigger: {
-                trigger: ".gallery",
-                start: "top top",
-                end: "bottom bottom",
-                scrub: 0.9,
-                markers: true,
-                pin:'.right'
+        <Tween
+          to={{
+            scrollTrigger: {
+              trigger: ".gallery",
+              start: "top top",
+              end: "bottom bottom",
+              scrub: 0.9,
+              markers: true,
+              pin: ".left",
+              onToggle: ({ isActive }) => {
+                setIsTriggerActive(isActive); // Update the state
               },
-            }}
-          >
-            
-            <div className="image h-80 w-80 bg-blue-500"></div>
-       
-          </Tween>
+            },
+          }}
+        >
+          <div className="flex justify-between gallery">
+            <div className="detailsWrapper left w-[50%] ">
+              <div className="details h-[100vh] flex ">
+                <div className="flex flex-col justify-between py-10">
+                  <div className="">
+                    <span className=" font-extrabold md:text-3xl text-2xl">
+                      01
+                    </span>
+                  </div>
+                  <div className="pl-10">
+                    <h2 className="md:text-4xl font-bold">Learn CS For Free</h2>
+                    <div className="flex flex-row items-center py-6">
+                      <div className="mr-4">
+                        <span className="text-2xl font-semibold">React JS</span>
+                      </div>
+                      <div className="ml-4">
+                        <span className="text-2xl font-semibold">Redux</span>
+                      </div>
+                    </div>
+                    <div className="">
+                      <p className="w-[70%] text-[20px]">
+                        An aplication that gathers the best free videos from the
+                        web especially youtube to create a structured curriculum
+                        for people to learn computer science!
+                      </p>
+                    </div>
+                    <div className="flex items-center py-4">
+                      <div className="mr-4">
+                        <Link
+                          className=" underline underline-offset-2 font-light"
+                          href={"https://github.com"}
+                          target="__blank"
+                        >
+                          Souce Code
+                        </Link>
+                      </div>
+                      <div className="ml-4">
+                        <Link
+                          className=" underline underline-offset-2 font-light"
+                          href={"https://google.com"}
+                          target="__blank"
+                        >
+                          Live
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="">
+                    <span className="font-bold md:text-3xl text-2xl">
+                      WEB APP
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="right  ">
+              <div className="h-[100vh] flex items-center">
+                <div className="image h-80 w-80 bg-blue-500"></div>
+              </div>
+              <div className="h-[100vh] flex items-center">
+                <div className="image h-80 w-80 bg-blue-500"></div>
+              </div>
+              <div className="h-[100vh] flex items-center">
+                <div className="image h-80 w-80 bg-blue-500"></div>
+              </div>
+              <div className="h-[100vh] flex items-center">
+                <div className="image h-80 w-80 bg-blue-500"></div>
+              </div>
+              <div className="h-[100vh] flex items-center">
+                <div className="image h-80 w-80 bg-blue-500"></div>
+              </div>
+              <div className="h-[100vh] flex items-center">
+                <div className="image h-80 w-80 bg-blue-500"></div>
+              </div>
+              <div className="h-[100vh] flex items-center">
+                <div className="image h-80 w-80 bg-blue-500"></div>
+              </div>
+              <div className="h-[100vh] flex items-center">
+                <div className="image h-80 w-80 bg-blue-500"></div>
+              </div>
+              <div className="h-[100vh] flex items-center">
+                <div className="image h-80 w-80 bg-blue-500"></div>
+              </div>
+            </div>
           </div>
-        </div>
+        </Tween>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
